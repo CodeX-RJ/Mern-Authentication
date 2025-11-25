fa1553c45c51b597f7e61950cdcbb347ad7b5346
Ramakant Jai <ramakantjai91@gmail.com>
2025-11-25 13:43:59 +0530

    corrected jwt middleware

--- Backend/middlwares/jwtTokenVerficaiton.js
+++ Backend/middlwares/jwtTokenVerficaiton.js
@@ -11,7 +11,7 @@ export const jwtTokenVerification = async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-    const user = await User.findById(decoded.userId).select("name email isEmailVerified");
+    const user = await User.findById(decoded.userId).select("name email isVerified");
 
     if (!user) {
       return res.status(401).json({ success: false, message: "Invalid token" });
